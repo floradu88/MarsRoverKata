3bdadfd7b9ef044ef109266aadb63b1f080463c5
Florin Radu <flo.radu_88@yahoo.com>
2014-03-30 10:37:47 +0300

    First step: project structure definition

---
+++ MarsRoverKata/MarsRoverKata.UnitTest/MarsRoverKataApiTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MarsRoverKata.UnitTest
+{
+    [TestClass]
+    public class MarsRoverKataApiTest
+    {
+        [TestMethod]
+        public void MarsRoverKataApiTest_InitializeConstructorWithStartingPosition()
+        {
+        }
+    }
+}
